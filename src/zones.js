9b6e05260c1d73eeb68d7754158bee31a29aad9e
Nhan Doan <nhandev88@gmail.com>
2018-07-14 07:27:03 +0700

    Create zones file

---
+++ src/zones.js
@@ -0,0 +1,28 @@
+const zones = {
+  3413829: {
+    country: 'Iceland',
+    timezone: 'Atlantic/Reykjavik'
+  },
+  2643743: {
+    country: 'Great Britain',
+    timezone: 'Europe/London'
+  },
+  2618425: {
+    country: 'Denmark',
+    timezone: 'Europe/Copenhagen'
+  },
+  745044: {
+    country: 'Turkey',
+    timezone: 'Europe/Istanbul'
+  },
+  292223: {
+    country: 'UAE',
+    timezone: 'Asia/Dubai'
+  },
+  2163355: {
+    country: 'Australia',
+    timezone: 'Australia/Hobart'
+  }
+}
+
+export default zones
