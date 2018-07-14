54f05e897f1c4c513d6f0ccc398be680702deb84
Nhan Doan <nhandev88@gmail.com>
2018-07-14 11:53:29 +0700

    Update
    - City details toggle
    
    - Return background colour based on temperature

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,8 +7,30 @@
     <div
       class="city"
       v-for="(city, index) in cities"
-      :key="index">
-      {{city}}
+      @click="toggleCity(city)"
+      :key="index"
+      :style="colourByTemp(city.main.temp)">
+      <h2 class="city-temp">{{city.main.temp | tempFormat}}</h2>
+      <h1 class="city-name">{{city.name}}<br>{{city.id | countryName}}</h1>
+      <ul
+        class="city-details"
+        v-if="city.opened">
+        <li>sunrise: {{new Date(city.sys.sunrise * 1000)}}</li>
+        <li>sunset: {{new Date(city.sys.sunset * 1000)}}</li>
+        <li>
+          <span
+            v-for="(condition, index) in city.weather"
+            :key="index">
+            {{condition.description}}
+          </span>
+        </li>
+        <li>current: {{city.main.temp | tempFormat}}</li>
+        <li>min: {{city.main.temp_min | tempFormat}}</li>
+        <li>max: {{city.main.temp_max | tempFormat}}</li>
+        <li>humidity: {{city.main.humidity}}%</li>
+        <li>visibility: {{Math.round(city.visibility / 1000)}} km</li>
+        <li>wind speed: {{city.wind.speed}} kph</li>
+      </ul>
     </div>
   </transition-group>
 </template>
@@ -16,6 +38,7 @@
 <script>
 import { getApi } from '../api'
 import citiesData from '../cities'
+import zones from '../zones'
 
 const API_KEY = '0aa2021d96650cdfe3518911b15684b6'
 const CITIES = '3413829,2643743,2618425,745044,292223,2163355'
@@ -41,6 +64,62 @@ export default {
           return item
         })
       })
+  },
+  methods: {
+    toggleCity (city) {
+      const isOpened = city ? city.opened : false
+
+      this.cities.map(item => {
+        item.opened = false
+      })
+
+      if (city) {
+        city.opened = !isOpened
+      }
+    },
+    colourByTemp (temp) {
+      // blue: hsl(240, 100%, 50%)
+      // yellow: hsl(60, 100%, 50%)
+
+      let hue = 0
+      const minHue = 240
+      const maxHue = 60
+      const hueRange = minHue - maxHue
+      const saturation = 100
+      const lightness = 50
+
+      const minTemp = 0
+      const maxTemp = 40
+      const tempRange = maxTemp - minTemp
+
+      if (temp <= minTemp) {
+        hue = minHue
+      } else if (temp >= maxTemp) {
+        hue = maxHue
+      } else {
+        hue = minHue - ((temp) * hueRange / tempRange)
+      }
+
+      let color = hue >= 200 ? 'white' : 'black'
+
+      let styleValue = {
+        backgroundColor: `hsl(${Math.round(hue)}, ${saturation}%, ${lightness}%)`,
+        color: color
+      }
+
+      return styleValue
+    }
+  },
+  filters: {
+    tempFormat (temp) {
+      return `${Math.round(temp)} ºC`
+    },
+    countryName (id) {
+      return zones[id]['country']
+    },
+    timezone (id) {
+      return zones[id]['timezone']
+    }
   }
 }
 </script>
